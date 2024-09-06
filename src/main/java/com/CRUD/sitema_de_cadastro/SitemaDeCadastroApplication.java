4425211404d8b30c9f019497726342ba178c791e
Bruno Alcantara <bruno.alcantara@hivecloud.com.br>
2024-09-06 15:49:01 -0300

    frist cmmit

---
+++ src/main/java/com/CRUD/sitema_de_cadastro/SitemaDeCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.CRUD.sitema_de_cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemaDeCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemaDeCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CRUD/sitema_de_cadastro/SitemaDeCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CRUD.sitema_de_cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemaDeCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
